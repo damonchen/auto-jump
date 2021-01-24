89200fc687b3f73e7171a6254ef72c046677d81c
damon <netubu@gmail.com>
2021-01-24 12:38:41 +0800

    fix: 支持知乎、微博等的页面跳转

---
+++ content-script.js
@@ -0,0 +1,32 @@
+// 必须设置run_at=document_start才能执行
+document.addEventListener('DOMContentLoaded', function() {
+    // 注入自定义JS
+    // injectCustomJs();
+    var host = location.host;
+    if(host === 'link.zhihu.com') {
+        var button = document.querySelector('.actions > a');
+        if(!!button) {
+            button.click();
+        }
+    } else if (host === 't.cn') {
+        var button = document.querySelector('.open-url > a');
+        if(!!button) {
+            button.click();
+        }
+    }
+})
+
+
+function injectCustomJs(jsPath) {
+    jsPath = jsPath || 'js/inject.js';
+    var temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+    temp.src = chrome.extension.getURL(jsPath);
+    // temp.onload = function()
+    // {
+    //     // 放在页面不好看，执行完后移除掉
+    //     this.parentNode.removeChild(this);
+    // };
+    document.body.appendChild(temp);
+}
